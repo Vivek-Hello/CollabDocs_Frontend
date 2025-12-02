a02cc31c0c0f9a593518505ba5660d249164b878
Vivek-Hello <vickyvivek391@gmail.com>
2025-12-02 17:54:57 +0530

    changing the config file

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import env from "@/types/localEnv";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // <-- This is at the root!
+  },
   async rewrites() {
     return [
       {
         source: "/api/:path*",
         destination: `${env.baseURL}/api/:path*`, // Proxy to backend
+        // If env.baseURL is undefined, fallback to process.env.BASE_URL!
+        // destination: `${process.env.BASE_URL}/api/:path*`
       },
     ];
   },
